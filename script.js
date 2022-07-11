b587c27e7a00eaaa17a64c6784a99d59b41c26c6
Nyi Zin Thant <codebynyi@gmail.com>
2022-07-11 16:56:12 +0630

    account login feature

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const account1 = {
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+  username: 'js',
 };
 
 const account2 = {
@@ -17,6 +18,7 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+  username: 'jd',
 };
 
 const account3 = {
@@ -24,6 +26,7 @@ const account3 = {
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
+  username: 'stw',
 };
 
 const account4 = {
@@ -31,6 +34,7 @@ const account4 = {
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
+  username: 'ss',
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -82,87 +86,41 @@ const displayBlance = function (movements) {
   labelBalance.textContent = `${balance} EUR`;
 };
 
-const displaySummary = function (movements) {
-  const income = movements
+const displaySummary = function (account) {
+  const income = account.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => (acc += mov), 0);
   labelSumIn.textContent = `${income} €`;
 
-  const outcome = movements
+  const outcome = account.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => (acc += mov), 0);
   labelSumOut.textContent = `${Math.abs(outcome)} €`;
 
-  const interest = movements
+  const interest = account.movements
     .filter(mov => mov > 0)
-    .map(mov => (mov * 1.2) / 100)
+    .map(mov => (mov * account.interestRate) / 100)
     .filter(mov => mov >= 1)
     .reduce((acu, mov) => acu + mov, 0);
   labelSumInterest.textContent = `${Math.abs(interest)} €`;
 };
 
-displaySummary(account1.movements);
-
-displayBlance(account1.movements);
-
-displayMovement(account1.movements);
-
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
-
-// const currencies = new Map([
-//   ['USD', 'United States dollar'],
-//   ['EUR', 'Euro'],
-//   ['GBP', 'Pound sterling'],
-// ]);
-
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-// const eurToUsd = 1.1;
-
-// const usdMov = movements.map(mov => mov * eurToUsd);
-
-// const withDrawal = movements.filter(function (key) {
-//   return key <= 0;
-// });
-
-// console.log(withDrawal);
-
-/////////////////////////////////////////////////
-
-// Let's go back to Julia and Kate's study about dogs. This time, they want to convert
-// dog ages to human ages and calculate the average age of the dogs in their study.
-// Your tasks:
-// Create a function 'calcAverageHumanAge', which accepts an arrays of dog's
-// ages ('ages'), and does the following things in order:
-// 1. Calculate the dog age in human years using the following formula: if the dog is
-// <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old,
-// humanAge = 16 + dogAge * 4
-// 2. Exclude all dogs that are less than 18 human years old (which is the same as
-// keeping dogs that are at least 18 years old)
-// 3. Calculate the average human age of all adult dogs (you should already know
-// from other challenges how we calculate averages �)
-// 4. Run the function for both test datasets
-// Test data:
-// § Data 1: [5, 2, 4, 1, 15, 8, 3]
-// § Data 2: [16, 6, 10, 5, 6, 1, 4]
-
-// const calcAverageHumanAge = function (ages) {
-//   const dogAgeToHuman = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
-//   const over18 = dogAgeToHuman.filter(age => age > 18);
-//   console.log(over18);
-//   const avgAge = over18.reduce((acc, age) => (acc += age)) / over18.length;
-//   console.log(avgAge);
-// };
-
-// calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
-
-const calcAverageHumanAge = function (ages) {
-  return ages
-    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
-    .filter(age => age > 18)
-    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
-};
-
-console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+// Event handler
+let cAccount;
+btnLogin.addEventListener('click', function (e) {
+  e.preventDefault();
+  cAccount = accounts.find(acc => acc.username === inputLoginUsername.value);
+  if (cAccount?.pin === Number(inputLoginPin.value)) {
+    // Clear input fields
+    inputLoginPin.value = inputLoginUsername.value = '';
+    // Display UI messages
+    labelWelcome.textContent = `Welcome back, ${cAccount.owner}`;
+    containerApp.style.opacity = 1;
+    // Display Movements
+    displayMovement(cAccount.movements);
+    // Display Balance
+    displayBlance(cAccount.movements);
+    // Display Summary
+    displaySummary(cAccount);
+  }
+});
